f42ef565f57dd0632b9196a61c40dc5391c9d54c
Erik Hesselink <erik@silkapp.com>
2013-05-16 10:38:58 +0000

    TraceKit: parse constructors in Chrome stack.
    Stack line looks like this:
        at new KeyValue (http://test.erik.silk/s/static/product.js:53062:6)

--- vendor/assets/javascripts/squash_javascript/tracekit.js
+++ vendor/assets/javascripts/squash_javascript/tracekit.js
@@ -546,7 +546,7 @@ TraceKit.computeStackTrace = (function () {
 			return null;
 		}
 
-		var chrome = /^\s*at (\S+) \(((?:file|http):.*?):(\d+)(?::(\d+))?\)\s*$/i,
+		var chrome = /^\s*at (?:new )?(\S+) ?\(((?:file|http):.*?):(\d+)(?::(\d+))?\)\s*$/i,
 			gecko = /^\s*(\S*)(?:\((.*?)\))?@((?:file|http).*?):(\d+)(?::(\d+))?\s*$/i,
 			lines = ex.stack.split("\n"),
 			stack = [],
